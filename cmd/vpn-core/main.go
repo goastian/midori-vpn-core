01a279508333a02a49004f35f4db765d67c18709
Alfonso Hernandez <alfonso.hernandez@astian.org>
2026-02-28 01:52:32 -0500

    feat: initial HTTP server bootstrap for midori-vpn-core

---
+++ cmd/vpn-core/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/goastian/midori-vpn-core/internal/api"
+	"github.com/goastian/midori-vpn-core/internal/config"
+	"github.com/goastian/midori-vpn-core/internal/wg"
+)
+
+func main() {
+	cfg := config.Load()
+
+	manager, err := wg.NewManager(cfg)
+	if err != nil {
+		log.Fatalf("failed to initialize WireGuard manager: %v", err)
+	}
+	defer manager.Close()
+
+	router := api.NewRouter(cfg, manager)
+
+	addr := fmt.Sprintf(":%s", cfg.Port)
+	log.Printf("astian-vpn-core listening on %s", addr)
+
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: router,
+	}
+
+	// Graceful shutdown
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+		<-sigCh
+		log.Println("shutting down...")
+		srv.Close()
+	}()
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("server error: %v", err)
+	}
+}
