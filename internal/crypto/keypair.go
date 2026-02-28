717d4e9dfd0fa193fe36092a2683be48f654a8ca
Alfonso Hernandez <alfonso.hernandez@astian.org>
2026-02-28 01:37:11 -0500

    generate keys spec Curve25519

---
+++ internal/crypto/keypair.go
@@ -0,0 +1,35 @@
+package crypto
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+
+	"golang.org/x/crypto/curve25519"
+)
+
+type Keypair struct {
+	PrivateKey string `json:"private_key"`
+	PublicKey  string `json:"public_key"`
+}
+
+func GenerateKeypair() (*Keypair, error) {
+	var privKey [32]byte
+	if _, err := rand.Read(privKey[:]); err != nil {
+		return nil, err
+	}
+
+	// Clamp private key per WireGuard/Curve25519 spec
+	privKey[0] &= 248
+	privKey[31] &= 127
+	privKey[31] |= 64
+
+	pubKey, err := curve25519.X25519(privKey[:], curve25519.Basepoint)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Keypair{
+		PrivateKey: base64.StdEncoding.EncodeToString(privKey[:]),
+		PublicKey:  base64.StdEncoding.EncodeToString(pubKey),
+	}, nil
+}
