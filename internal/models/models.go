6ad7f5fd31f282654ca74f443dafa23dff5a2bb1
Alfonso Hernandez <alfonso.hernandez@astian.org>
2026-02-28 06:14:40 -0500

    update fields

--- internal/models/models.go
+++ internal/models/models.go
@@ -7,12 +7,16 @@ import (
 )
 
 type User struct {
-	ID           uuid.UUID `json:"id"`
-	AuthentikUID string    `json:"authentik_uid"`
-	Email        string    `json:"email"`
-	Groups       []string  `json:"groups"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	ID           uuid.UUID  `json:"id"`
+	AuthentikUID string     `json:"authentik_uid"`
+	Email        string     `json:"email"`
+	DisplayName  string     `json:"display_name"`
+	Groups       []string   `json:"groups"`
+	IsBanned     bool       `json:"is_banned"`
+	BannedAt     *time.Time `json:"banned_at,omitempty"`
+	BanReason    string     `json:"ban_reason,omitempty"`
+	CreatedAt    time.Time  `json:"created_at"`
+	UpdatedAt    time.Time  `json:"updated_at"`
 }
 
 type VPNServer struct {
@@ -39,6 +43,7 @@ type Peer struct {
 	PublicKey     string     `json:"public_key"`
 	AssignedIP    string     `json:"assigned_ip"`
 	IsActive      bool       `json:"is_active"`
+	DeviceName    string     `json:"device_name"`
 	LastHandshake *time.Time `json:"last_handshake,omitempty"`
 	BytesSent     int64      `json:"bytes_sent"`
 	BytesReceived int64      `json:"bytes_received"`
@@ -53,4 +58,24 @@ type AuditLog struct {
 	Metadata  map[string]interface{} `json:"metadata"`
 	IPAddress string                 `json:"ip_address"`
 	CreatedAt time.Time              `json:"created_at"`
+}
+
+type ConnectionConfig struct {
+	PeerID          uuid.UUID `json:"peer_id"`
+	PeerIP          string    `json:"peer_ip"`
+	ServerPublicKey string    `json:"server_public_key"`
+	ServerEndpoint  string    `json:"server_endpoint"`
+	DNS             string    `json:"dns"`
+	AllowedIPs      string    `json:"allowed_ips"`
+}
+
+type AdminStats struct {
+	TotalUsers        int   `json:"total_users"`
+	TotalServers      int   `json:"total_servers"`
+	ActiveServers     int   `json:"active_servers"`
+	TotalPeers        int   `json:"total_peers"`
+	ActivePeers       int   `json:"active_peers"`
+	TotalSubscriptions int  `json:"total_subscriptions"`
+	TotalBytesSent    int64 `json:"total_bytes_sent"`
+	TotalBytesRecv    int64 `json:"total_bytes_received"`
 }
